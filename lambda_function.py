56feb00976e05e51ace02b4faf9bc6e5c59b9cc6
Ivan <ivan.lin1993@gmail.com>
2018-10-08 02:11:48 +0800

    fix

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,13 @@
 import json
 import boto3
-from dependency import kubernetes as k8s
+import kubernetes as k8s
 import os
 import yaml
 
 
 def lambda_handler(event, context):
     # TODO implement
+    main()
     return {
         "statusCode": 200,
         "body": json.dumps('Hello from Lambda!')
@@ -15,21 +16,35 @@ def lambda_handler(event, context):
 def download_kubeconfig():
     try:
         s3 = boto3.resource('s3')
-        s3.Object(os.environ['S3_BUCKET'], os.environ['KUBECONFIG_FILE']).download_file('/tmp/kubeconfig')
+        s3.Object(os.environ['S3_BUCKET'],os.environ['CLUSTER_NAME'] + '/' + os.environ['KUBECONFIG_FILE']).download_file('/tmp/kubeconfig')
     except Exception as e:
         print(e)
         raise
     return '/tmp/kubeconfig'
 
 
+
+
+
 def load_deploy_config():
     try:
         s3 = boto3.resource('s3')
-        res = json.loads(s3.Object(os.environ['S3_BUCKET'], os.environ['DEPLOY_CONFIG']).get()['Body'].read().decode())
+        res = json.loads(s3.Object(os.environ['S3_BUCKET'], os.environ['CLUSTER_NAME'] + '/' + os.environ['DEPLOY_CONFIG']).get()['Body'].read().decode())
     except Exception as e:
         print(e)
         raise
     return res
+
+
+def load_deploy_yml():
+    try:
+        s3 = boto3.resource('s3')
+        res = s3.Object(os.environ['S3_BUCKET'], os.environ['CLUSTER_NAME'] + '/' + os.environ['DEPLOYFILE']).get()['Body'].read().decode()
+    except Exception as e:
+        print(e)
+        raise
+    return res
+
 
 
 def setting_kubefile():
@@ -39,15 +54,13 @@ def setting_kubefile():
 
 def main():
     setting_kubefile()
-    delploy_config = load_deploy_config()
-    deployfile = delploy_config['deployment_file']
+    delploy_file = load_deploy_yml()
     client = k8s.client.CoreApi()
-    with open(deployfile) as f:
-        dep = yaml.load(f)
-        k8s_beta = k8s.client.ExtensionsV1beta1Api()
-        resp = k8s_beta.create_namespaced_deployment(
-            body=dep, namespace="default")
-        print("Deployment created. status='%s'" % str(resp.status))
+    delploy_yml = yaml.load(delploy_file)
+    k8s_beta = k8s.client.ExtensionsV1beta1Api()
+    resp = k8s_beta.create_namespaced_deployment(
+        body=delploy_yml, namespace="default")
+    print("Deployment created. status='%s'" % str(resp.status))
 
 
 if __name__ == '__main__':
